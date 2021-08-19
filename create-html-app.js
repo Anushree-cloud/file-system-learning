b4a40d317d44f48b3df1780ac72781cf156eae9d
unknown <chakrabortyanushree1999@gmail.com>
2021-08-19 12:32:58 +0530

    resolving errors

--- create-html-app.js
+++ create-html-app.js
@@ -46,27 +46,36 @@ const reader = readline.createInterface({
 
 
 
-function htmlFileContent(title){
+function htmlFileContent(title, css = null, script = null, bootstrap = null){
     return `<html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>${title}</title>
-            
+            ${css ? addLinkTag(css) : null}
+            ${bootstrap ? addBootstrap() : null}
         </head>
         <body>
-            
+            ${script ? addScriptTag(script) : null}
         </body>
         </html>`
 }
 
 function addScriptTag(source){
-    return `<script src="${source}"></script>`
+    return `<script src="./js/${source}"></script>`
 }
 
 function addLinkTag(fileLocation){
-    return `<link rel="stylesheet" src="${fileLocation}" />`
+    return `<link rel="stylesheet" href="./css/${fileLocation}" />`
+}
+
+function addBootstrap(){
+    return `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>`
 }
 
 function createFolder(folderName, fileName){
@@ -79,77 +88,77 @@ function createFolder(folderName, fileName){
     })
 }
 
-function createFile(projectName, fileName){
-    fs.writeFile(`./${projectName}/${fileName}`, "/* Add your code here.*/", () => {
+// function addContent(projectName, folderName){
+//     switch (folderName) {
+//         case projectName:
+//             htmlFileContent(projectName)
+//             break;
+        
+//         case "css":
+//             htmlFileContent(projectName, )
+
+//         default:
+//             break;
+//     }
+    
+// }
+
+function createFile(projectName, folderName = null, fileName){
+    fs.writeFile(`./${projectName}/${folderName ? `${folderName}/` : null}${fileName}`, htmlFileContent(projectName), () => {
             console.log(`${fileName} file created inside the folder ${projectName}.`)
         })
 }
 
-function toContinue() {
-    reader.question('\nDo you want to continue (y/n): ', function(toQuit) {
-        switch (toQuit) {
-            case 'y':
-                todoApp()
-                break;
-            case 'n':
-                reader.close()
-            default:
-                console.log('\nInvalid entry!');
-                todoApp()
-                break;
-        }
-    })
-}
 
+function addFiles() {
+    console.clear()
+    reader.question(`\nDo you want to add files? (y/n): `, function(addFile){
+
+        if(addFile === 'y'){
+            console.log(`\nAdd Css file: Press 1\nAdd JS file: Press 2\nAdd Bootstrap file: Press 3`);
+
+            reader.question(`Enter your choice: `, function(fileType){
+
+                switch (fileType) {
+
+                    case '1':
+                            reader.question(`Enter your CSS filename with extension: `, function(fileName){
+                                createFolder("css", fileName)
+                            })
+                            addFiles()
+                            break;
+                    
+                    case '2':
+                            reader.question(`Enter your JS filename with extension: `, function(fileName){
+                                createFolder("js", fileName)
+                            })
+                            addFiles()
+                            break;
+
+                    case '3':
+                            console.log(`\nBootstrap CDNs Added!`);
+                            addFiles()
+                            break;
+
+                    default:
+                        break;
+                }
+                
+            })
+        }
+        else{
+            reader.close()
+        }
+    })    
+}
 
 function createFileSystem(){
     console.clear()
     console.log("Welcome to your File-System.\nYou can add CSS and JavaScript files.\nYou can include Bootstap.");
 
     reader.question('\nEnter your Project name: ', function(projectName){
-
         createFolder(projectName, "index.html")
-        htmlFileContent(projectName)
-
-        reader.question(`\nDo you want to add files? (y/n): `, function(addFile){
-
-            if(addFile === 'y'){
-                console.log(`\nAdd Css file: Press 1\nAdd JS file: Press 2\nAdd Bootstrap file: Press 3`);
-
-                reader.question(`Enter your choice: `, function(fileType){
-                    
-                    switch (fileType) {
-
-                        case '1':
-                                reader.question(`Enter your CSS filename with extension`, function(fileName){
-                                    createFolder("css", fileName)
-                                })
-                                toContinue()
-                                break;
-                        
-                        case '2':
-                                reader.question(`Enter your JS filename with extension`, function(fileName){
-                                    createFolder("js", fileName)
-                                })
-                                toContinue()
-                                break;
-
-                        case '3':
-                                console.log(`\nBootstrap CDNs Added!`);
-                                toContinue()
-                                break;
-
-                        default:
-                            break;
-                    }
-                    
-                })
-            }
-            else{
-                reader.close()
-            }
-        })
-        toContinue()
+        addFiles()
     })
 }
 
